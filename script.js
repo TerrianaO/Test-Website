1139d12884d6540b26c4d1742da742b765ca54e0
Terriana Odom <77416739+TerrianaO@users.noreply.github.com>
2021-05-08 01:32:10 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+$(".link").mouseenter(function() {
+    $(".link").css("color", "pink");
+});
+
+$(".git").mouseenter(function() {
+    $(".git").css("color", "pink");
+});
+
+$(".insta").mouseenter(function() {
+    $(".insta").css("color", "pink");
+});
+
+$(".face").mouseenter(function() {
+    $(".face").css("color", "pink");
+});
+
+$("a").mouseleave(function() {
+    $("a").css("color", "black");
+});
