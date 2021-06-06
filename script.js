574a07e611c3dfa608c22f7003f4dd4d4ddbbffe
Terriana Odom <77416739+TerrianaO@users.noreply.github.com>
2021-06-05 19:18:12 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// the starting links for social media
 $(".link").mouseenter(function() {
     $(".link").css("color", "pink");
 });
@@ -14,6 +15,23 @@ $(".face").mouseenter(function() {
     $(".face").css("color", "pink");
 });
 
+// the start for the game links
+$(".goldMiner").mouseenter(function() {
+    $(".goldMiner").css("color", "pink");
+});
+
+$(".poptropica").mouseenter(function() {
+    $(".poptropica").css("color", "pink");
+});
+
+$(".chess").mouseenter(function() {
+    $(".chess").css("color", "pink");
+});
+
+$(".sugarSugar").mouseenter(function() {
+    $(".sugarSugar").css("color", "pink");
+});
+
 $("a").mouseleave(function() {
     $("a").css("color", "black");
-});
+}); // after the mouse is off any link it will turn black bc of this one 
