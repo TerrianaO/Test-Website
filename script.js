6024115180afa234ef81e053e9f2613b31cf9c8a
Terriana Odom <77416739+TerrianaO@users.noreply.github.com>
2021-06-05 20:59:34 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -32,31 +32,39 @@ $(".sugarSugar").mouseenter(function() {
     $(".sugarSugar").css("color", "pink");
 });
 
-$(".sugarSugar").mouseenter(function() {
-    $(".sugarSugar").css("color", "pink");
+$(".appleShooter").mouseenter(function() {
+    $(".appleShooter").css("color", "pink");
 });
 
-$(".sugarSugar").mouseenter(function() {
-    $(".sugarSugar").css("color", "pink");
+$(".littleAlchemy").mouseenter(function() {
+    $(".littleAlchemy").css("color", "pink");
 });
 
-$(".sugarSugar").mouseenter(function() {
-    $(".sugarSugar").css("color", "pink");
+$(".lemonadeStand").mouseenter(function() {
+    $(".lemonadeStand").css("color", "pink");
 });
 
-$(".sugarSugar").mouseenter(function() {
-    $(".sugarSugar").css("color", "pink");
+$(".bloxorz").mouseenter(function() {
+    $(".bloxorz").css("color", "pink");
 });
 
-$(".sugarSugar").mouseenter(function() {
-    $(".sugarSugar").css("color", "pink");
+$(".2048").mouseenter(function() {
+    $(".2048").css("color", "pink");
 });
 
-$(".sugarSugar").mouseenter(function() {
-    $(".sugarSugar").css("color", "pink");
+$(".checkers").mouseenter(function() {
+    $(".checkers").css("color", "pink");
+});
+
+$(".hardGame").mouseenter(function() {
+    $(".hardGame").css("color", "pink");
+});
+
+$(".parkingGame").mouseenter(function() {
+    $(".parkingGame").css("color", "pink");
 });
 
 // after the mouse thingy is off any link it, will turn black bc of this one 
 $("a").mouseleave(function() {
     $("a").css("color", "black");
-}); 
+});
